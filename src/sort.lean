1b463e9f44ab7d7dd9d5001563b6f2db8e44094b
Mascha van der Marel <43924750+MaschavanderMarel@users.noreply.github.com>
2021-11-02 18:50:57 +0100

    add examples of trans linear and total

--- src/sort.lean
+++ src/sort.lean
@@ -32,18 +32,20 @@ begin
     refl }
 end 
 
-def list.to_set : list α → set α --source: chapter 10.6 Theorem Proving in Lean
+def list.to_set : list α → set α --Source: chapter 10.6 Theorem Proving in Lean.
 | []     := ∅
 | (h::t) := {h} ∪ list.to_set t
 
+-- This is predefined in Functional Algorithms Verified.
 def multiset.to_set: multiset α → set α :=  
 λ m: multiset α, {x: α | x ∈ m }
 
+-- This lemma is predefined in the multiset file of Isabelle and used to prove lemma set_insort.
 lemma set_mset_mset: multiset.to_set ↑xs = list.to_set xs := 
 begin
   induction' xs,
   { refl},
-  { simp [list.to_set, multiset.to_set,← ih, multiset.to_set, set.insert_def] }
+  { simp [list.to_set,← ih, multiset.to_set, set.insert_def] }
 end 
 
 lemma set_insort [decidable_rel r]:  (ordered_insert r x xs).to_set  = {x} ∪ xs.to_set  :=
@@ -51,6 +53,7 @@ begin
   simp [set.insert_def, ← set_mset_mset, mset_insort, multiset.to_set],
 end
 
+-- This definition follows the definition in Functional Algorithms Verified.
 def is_sorted [is_total α r] [is_trans α r] : list α → Prop 
 | [] := true
 | (h::t) := (∀ y ∈ t.to_set, r h y ) ∧ is_sorted t
@@ -69,8 +72,8 @@ begin
       intros y h3, 
       have h4: y ∈ xs.to_set → r hd y, from h1 y,
       have h5: r hd y, from h4 h3,
-      exact @trans α r _inst_3 x hd y h h5 , 
-      },
+      clear h4 h1 h2 _inst_1 _inst_2 ih h3 xs,
+      exact @trans α r _inst_3 x hd y h h5 }, --why is _inst_3 not found by Lean without naming it explicitly even after clearing all other hypotheses?
     { simp [is_sorted, list.to_set, ih, set_insort],
       intros h1 h2,
       have h3: r hd x ∨ r x hd, from @total_of α r _inst_2 hd x, 
@@ -83,3 +86,19 @@ begin
   { simp},
   { simp [sorted_insort, ih] }
 end
+
+example (a b c: α ) [decidable_rel r] [is_trans α r] [is_total α r] [is_linear_order α r]: r a b ∧ r b c → r a c :=
+begin
+intro h1,
+have h2: r a b, from h1.left,
+have h3: r b c, from h1.right,
+exact @trans α r _ a b c h2 h3,
+-- exact trans h2 h3
+end
+
+example (a b : α ) [is_linear_order α r] : r a b ∨ r b a :=
+begin
+exact total_of r a b
+end
+
+
