c8a298eeb973654b241e5632f0c1c40e2d89b382
Mascha van der Marel <43924750+MaschavanderMarel@users.noreply.github.com>
2021-11-28 10:06:08 +0100

    def quicksort

--- src/quicksort.lean
+++ src/quicksort.lean
@@ -1,9 +1,6 @@
 import utilities
 
 open list
-open multiset
-open set
-open list.perm
 open nat
 
 set_option trace.simplify.rewrite true
@@ -15,16 +12,47 @@ variable xs: list α
 
 /- 
 # Quicksort
+
+The qsort from the library is *not* really quicksort since it doesn't partition the elements in-place.calc
+Therefore quicksort is defined below following the definition from Functional Algorithms Verified!.
  -/
 
-def quicksort : list ℕ  → list ℕ    
+lemma well_founded_qs [decidable_rel r]:  (list.filter (λ (y), r y x) xs).sizeof < 1 + xs.sizeof :=
+  begin
+    induction xs,
+    { simp},
+    simp [list.filter],
+    split_ifs,
+    {   simp [list.sizeof],
+        linarith },
+    simp [list.sizeof],
+    linarith
+  end
+
+lemma well_founded_qs' [decidable_rel r]:  (list.filter (λ (y),¬ r y x) xs).sizeof < 1 + xs.sizeof :=
+begin
+    induction xs,
+    {   simp},
+    simp [list.filter],
+    split_ifs,
+    {   simp [list.sizeof],
+        linarith },
+    simp [list.sizeof],
+    linarith,
+end
+
+def quicksort [decidable_rel r]: list α → list α     
 | [] := []
 | (x::xs) :=  
-    have (list.filter (λ (y : ℕ), y < x) xs).sizeof < 1 + x + xs.sizeof, from sorry,
-    have (list.filter (has_le.le x) xs).sizeof < 1 + x + xs.sizeof, from sorry,
-    quicksort (xs.filter (λ y: ℕ, y < x)) ++ [x] ++ quicksort (xs.filter (λ y: ℕ , y ≥ x))   
+  have (list.filter (λ (y), r y x) xs).sizeof < 1 + xs.sizeof, by apply well_founded_qs,
+  have (list.filter (λ (y : α), ¬r y x) xs).sizeof < 1 + xs.sizeof, by apply well_founded_qs',
+  quicksort (xs.filter (λ y: α, r y x)) ++ [x] ++ quicksort (xs.filter (λ y: α  , ¬ r y x))   
 
+#eval list.sizeof [1,1] 
+#eval sizeof 3
 
-#eval quicksort [3,2,1]
+#eval list.filter (λ x: ℕ , x > 1) [2,3,1]
+#check (λ x y: ℕ , x > y)
+#eval quicksort (λ n m :ℕ , ¬ n < m) [3,2,1]
 -- quicksort is defined in library
-#eval list.qsort (λ m n: ℕ , m < n) [2,1]
+#eval list.qsort (λ m n: ℕ , m > n) [2,1]
