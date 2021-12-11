f7d4effdfa334c74e335be5f16ef35c5d30f8a1c
Mascha van der Marel <43924750+MaschavanderMarel@users.noreply.github.com>
2021-12-11 17:10:42 +0100

    almost complete trees

---
+++ src/acomplete_binary_tree.lean
@@ -0,0 +1,18 @@
+import binary_tree
+
+set_option trace.simplify.rewrite true
+set_option trace.simp_lemmas true
+
+open tree
+
+variable {α : Type}
+variable a: α 
+variable t: tree α 
+
+/-
+# Almost Complete Binary Trees
+
+## Definition
+-/
+
+
