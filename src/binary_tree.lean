381541d704c6355f2910b22d9ea815cf0ee69a1c
Mascha van der Marel <43924750+MaschavanderMarel@users.noreply.github.com>
2021-12-19 10:57:18 +0100

    more lemmas tree size

--- src/binary_tree.lean
+++ src/binary_tree.lean
@@ -8,10 +8,15 @@ open tree
 
 variable {α : Type}
 variable a: α 
-variable t: tree α 
+variables t l r: tree α 
 
 /-
 ## Definitions
+
+A binary tree is already defined in Lean as tree. 
+The difference with __Functional Algorithms, Verified!__ is that a leaf is called nil, 
+and the order of the node is (value, tree, tree) instead of (tree, value, tree). 
+Since this does not change the structure of the proofs, the Lean definition is reused.
 -/
 
 def height : tree α → ℕ
@@ -30,7 +35,6 @@ def size1: tree α → ℕ
 | nil := 1
 | (node a l r) := size1 l + size1 r
 
-
 /-
 ## Lemmas size
 -/
@@ -42,13 +46,44 @@ begin
   cc
 end
 
+/- Only in Isabelle file -/
+@[simp]
+lemma size1_ge0: 0 < size1 t :=
+begin
+  simp [size1_size],
+end
+
+/- Only in Isabelle file -/
+@[simp]
+lemma eq_size_0 : size t = 0 ↔ t = nil :=
+begin
+  cases t,
+  repeat { simp [size] },
+end
+
+/- Only in Isabelle file -/
+@[simp]
+lemma eq_0_size: 0 = size t ↔ t = nil :=
+begin
+  cases t with a l r,
+  repeat { simp [size, nat.add_one, ← ne.def]},
+  apply ne.symm,
+  simp,
+end
+
+/- Only in Isabelle file -/
+lemma neq_nil_iff: t ≠ nil ↔ ∃ a l r, t = (node a l r) :=
+begin
+  sorry
+end  
+
+#eval size1 (node 1 nil nil)
+
 /-
 ## Lemmas height
 -/
 
-/-
-Only in Isabelle file
--/
+/- Only in Isabelle file -/
 @[simp]
 lemma eq_height_0 : height t = 0 ↔ t = nil :=
 begin
@@ -56,21 +91,17 @@ begin
   repeat { simp [height],},
 end
 
-/-
-Only in Isabelle file
--/
+/- Only in Isabelle file -/
 @[simp]
 lemma eq_0_height : 0 = height t ↔ t = nil :=
 begin
   cases t with a l r,
-  repeat { simp [height, nat.add_one]},
-  have: 0 < (max (height l) (height r)).succ, from nat.succ_pos (max (height l) (height r)),
-  linarith
+  repeat { simp [height, nat.add_one, ← ne.def]},
+  apply ne.symm,
+  simp,
 end
 
-/-
-Only in Isabelle file
--/
+/- Only in Isabelle file -/
 @[simp]
 lemma height_le_size_tree: height t <= size t :=
 begin
@@ -112,9 +143,7 @@ begin
     ... = 2 ^ (height r + 1) : by ring
 end
 
-/-
-Only in Isabelle file
--/
+/- Only in Isabelle file -/
 lemma size_height: size t <= 2 ^ height t - 1 :=
 begin
   have h: size1 t <= 2 ^ height t, from size1_height t,
@@ -124,8 +153,6 @@ begin
   have h3:size t + 1 ≤ 2 ^ height t ↔ size t ≤ 2 ^ height t - 1, from nat.add_le_to_le_sub (size t) h2,
   exact iff.elim_left h3 h,
 end
-
-#check @tsub_le_iff_right
 
 lemma min_height_size1: 2 ^ min_height t <= size1 t :=
 begin
